14e160b48ee898ce335872cc3e804e258d71a6f5
Fabio Lourenco <fabiolourenco91@gmail.com>
2018-05-11 01:32:07 -0700

    worked on FoodTracked

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -8,11 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    // MARK: Properties
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var mealLabel: UILabel!
+    
+    // MARK: Actions
+    @IBAction func btnLabelText(_ sender: UIButton) {
+        mealLabel.text="Default Text"
+    }
+    
+    @IBAction func selectImageFromGallery(_ sender: UITapGestureRecognizer) {
+        //Hide Keyboard
+        textField.resignFirstResponder()
+//      ImagePicker View
+        let imagePickerController = UIImagePickerController()
+//       photos in gallery only
+    imagePickerController.sourceType = .photoLibrary
+//        viewController is notified when image is selected
+        imagePickerController.delegate=self
+        present(imagePickerController, animated:true, completion: nil)
+        
+    }
+    //    MARK:UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        dismiss image selection
+        dismiss(animated:true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        multiple representations of an image. (use original)
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else
+        {
+            fatalError("Expected and image, the provided file was (file)")
+        }
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide Keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealLabel.text = textField.text
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Handle text Field user input through delegate
+        textField.delegate=self
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +67,3 @@ class ViewController: UIViewController {
 
 
 }
-
